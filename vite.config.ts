5e4040e81652eadf3c589b6f769eb684c633e548
mohitdudhatverselix <mohitdudhat22@gmail.com>
2024-07-28 21:16:10 +0530

    init commit

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import react  from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import { resolve, dirname } from 'path';
+
+// Resolve __dirname equivalent
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        background: resolve(__dirname, 'src/background.js')
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
+    }
+  }
+});
